31757403a6fc3b1d2edca88ba761429a61efc22f
Calin Crisan <ccrisan@gmail.com>
2021-11-25 23:25:26 +0200

    Fix QPIGS2 pv2_power missing property

--- qtoggleserver/mppsolar/commands/qpigs2.py
+++ qtoggleserver/mppsolar/commands/qpigs2.py
@@ -3,29 +3,25 @@ from .base import Command
 
 
 class QPIGS2(Command):
-    pass
+    REQUEST_FMT = 'QPIGS2'
 
 
 class QPIGS2_MAX(QPIGS2):
-    REQUEST_FMT = 'QPIGS2'
-
     RESPONSE_FMT = (
         '{pv2_current:f} '
         '{pv2_voltage:f} '
-        '{scc2_voltage:f} '
+        '{pv2_power:f}'
     )
 
     UNITS = {
         'pv2_current': 'A',
         'pv2_voltage': 'V',
-        'scc2_voltage': 'V',
         'pv2_power': 'W'
     }
 
     DISPLAY_NAMES = {
         'pv2_current': 'PV2 Current',
         'pv2_voltage': 'PV2 Voltage',
-        'scc2_voltage': 'SCC2 Voltage',
         'pv2_power': 'PV2 Power'
     }
 
